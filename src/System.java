0c1e894ee34724e26ac9102dbbe83843e834f648
rmolotok <roland.from.laptop@gmail.com>
2024-03-21 13:45:35 +0200

    System module

---
+++ src/System.java
@@ -0,0 +1,9 @@
+import java.io.PrintStream;
+
+public class System {
+    public static PrintStream out;
+
+    public static void main(String[] args) {
+        java.lang.System.out.println("Hi, system!");
+    }
+}
